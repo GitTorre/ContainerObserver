952c58646799dba2aa3712ec65716ecf50dfe403
Charles Torre <ctorre@microsoft.com>
2020-09-04 18:46:27 -0700

    Added ContainerObserverStartup impl

---
+++ ContainerObserver/ContainerObserverStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FabricObserver.Observers;
+
+[assembly: FabricObserver.FabricObserverStartup(typeof(ContainerObserver))]
+namespace FabricObserver.Observers
+{
+    public class ContainerObserverStartup : IFabricObserverStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(typeof(ObserverBase), typeof(ContainerObserver));
+        }
+    }
+}
